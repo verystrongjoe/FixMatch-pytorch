692155f30f7b0b97f528559995cfbf6e314f67c2
ukjo <verystrongjoe@gmail.com>
2023-01-21 18:46:13 +0900

    add sh

---
+++ run_n_augs.sh
@@ -0,0 +1,39 @@
+
+
+python -m train --project-name waferfix-230106 --wandb --dataset wm811k --proportion 0.5 --arch wideresnet --batch-size 64  --lr 0.01 --expand-labels --seed 1234 --out results/wm811k@0.5
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 3 --wandb  --dataset wm811k --proportion 0.5 --arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.5
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 4 --wandb  --dataset wm811k --proportion 0.5 --arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.5
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 5 --wandb  --dataset wm811k --proportion 0.5 --arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.5
+
+
+python -m train --project-name waferfix-230106 --wandb --dataset wm811k --proportion 0.25--arch wideresnet --batch-size 64  --lr 0.01 --expand-labels --seed 1234 --out results/wm811k@0.25
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 3 --wandb  --dataset wm811k --proportion 0.25--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.25
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 4 --wandb  --dataset wm811k --proportion 0.25--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.25
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 5 --wandb  --dataset wm811k --proportion 0.25--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.25
+
+
+python -m train --project-name waferfix-230106 --wandb --dataset wm811k --proportion 0.1--arch wideresnet --batch-size 64  --lr 0.01 --expand-labels --seed 1234 --out results/wm811k@0.1
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 3 --wandb  --dataset wm811k --proportion 0.1--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.1
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 4 --wandb  --dataset wm811k --proportion 0.1--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.1
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 5 --wandb  --dataset wm811k --proportion 0.1--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.1
+
+
+python -m train --project-name waferfix-230106 --wandb --dataset wm811k --proportion 0.05--arch wideresnet --batch-size 64  --lr 0.01 --expand-labels --seed 1234 --out results/wm811k@0.05
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 3 --wandb  --dataset wm811k --proportion 0.05--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.05
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 4 --wandb  --dataset wm811k --proportion 0.05--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.05
+
+python -m train --project-name waferfix-230106 --n-weaks-combinations 5 --wandb  --dataset wm811k --proportion 0.05--arch wideresnet --batch-size 64  --lr 0.003 --expand-labels --seed 1234 --out results/wm811k@0.05
+
+
+
--- run_supervised.sh
+++ run_supervised.sh
@@ -1,6 +1,10 @@
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 1.0 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 0.50 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 0.25  --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 0.1 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 0.05 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
-python -m train_supervised  --wandb --project-name wm811k-supervised-230109 --proportion 0.01 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised
+python -m train_supervised  --wandb --project-name wm811k-supervised-230115 --proportion 1.0 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-1.0
+python -m train_supervised  --wandb --project-name wm811k-supervised-230115 --proportion 0.50 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.50
+python -m train_supervised  --wandb --project-name wm811k-supervised-230115 --proportion 0.25  --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.25
+python -m train_supervised  --wandb --project-name wm811k-supervised-230115 --proportion 0.1 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.1
+python -m train_supervised  --wandb --project-name wm811k-supervised-230115 --proportion 0.05 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.05
+python -m train_supervised  - -wandb --project-name wm811k-supervised-230115 --proportion 0.01 --dataset wm811k --arch wideresnet --batch-size 64 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.01
+
+
+
+
