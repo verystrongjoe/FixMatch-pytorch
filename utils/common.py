6004ac84c96c5b001f9c2642e99e45b3e75204d7
user <verystrongjoe@gmail.com>
2023-02-21 21:42:05 +0900

    add sweep

--- utils/common.py
+++ utils/common.py
@@ -72,7 +72,7 @@ def get_args():
 
     # dataset
     parser.add_argument('--dataset', default='wm811k', type=str, choices=['wm811k', 'cifar10', 'cifar100'], help='dataset name')
-    parser.add_argument('--proportion', type=float, help='percentage of labeled data used', default=1.)
+    parser.add_argument('--proportion', type=float, help='percentage of labeled data used', required=True)
 
     parser.add_argument('--num_channel', type=int, default=2)
     parser.add_argument('--num_classes', type=int, default=8)
