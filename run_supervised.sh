4ec80d34385b4e155cb7ceceee1cea061b92ef29
user <verystrongjoe@gmail.com>
2023-03-12 23:05:40 +0900

    change supervised sh

--- run_supervised.sh
+++ run_supervised.sh
@@ -1,3 +1,7 @@
+# nohup ./run_supervised.sh > nohup_supervisd.out &
+# tail -f nohup_supervisd.out 
+
+
 CUDA_VISIBLE_DEVICES=1 python -m train_supervised --epochs 300 --gpus 1 --wandb --project-name waferfix-seed-check --proportion 1.0  --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-1.0
 CUDA_VISIBLE_DEVICES=1 python -m train_supervised --epochs 300 --gpus 1 --wandb --project-name waferfix-seed-check --proportion 0.50 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.50
 CUDA_VISIBLE_DEVICES=1 python -m train_supervised --epochs 300 --gpus 1 --wandb --project-name waferfix-seed-check --proportion 0.25 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.25
