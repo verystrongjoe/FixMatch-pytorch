daa9976fb0c1bdd6f9aaef5f617ed66b974949c2
user <verystrongjoe@gmail.com>
2023-03-12 23:03:26 +0900

    change supervised sh

--- run_supervised.bat
+++ run_supervised.bat
@@ -1,5 +1,5 @@
-python -m train_supervised --project-name temp  --gpus 0 --proportion 0.50 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.50
-python -m train_supervised --project-name temp  --gpus 0 --proportion 0.25 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.25
-python -m train_supervised --project-name temp  --gpus 0 --proportion 0.1  --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.1
-python -m train_supervised --project-name temp  --gpus 0 --proportion 0.05 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.05
+python -m train_supervised --project-name waferfix-seed-check  --gpus 1 --proportion 0.50 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.50
+python -m train_supervised --project-name waferfix-seed-check  --gpus 1 --proportion 0.25 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.25
+python -m train_supervised --project-name waferfix-seed-check  --gpus 1 --proportion 0.1  --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.1
+python -m train_supervised --project-name waferfix-seed-check  --gpus 1 --proportion 0.05 --dataset wm811k --arch resnet18 --batch-size 256 --lr 0.003 --expand-labels --seed 5 --out results/wm811k-supervised-0.05
 
