202deb7011ee6e23ec084472bf0bf5e3fb1ed7c8
user <verystrongjoe@gmail.com>
2023-04-16 23:03:38 +0900

    .

--- run_baseline.sh
+++ run_baseline.sh
@@ -2,6 +2,15 @@
 # tail -f nohup_baseline.out 
 
 
+for p in 0.05 0.1 0.25 0.5 1.0
+do
+    for k in 1 2 3 5
+    do
+        CUDA_VISIBLE_DEVICES=1 python -m train_baseline --K $k --project-name waferfix_baseline --proportion $p --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+    done
+done
+
+
 # CUDA_VISIBLE_DEVICES=0 python -m train_baseline --project-name waferfix_baseline --proportion 1.0  --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
 # CUDA_VISIBLE_DEVICES=0 python -m train_baseline --project-name waferfix_baseline --proportion 0.50 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
 # CUDA_VISIBLE_DEVICES=0 python -m train_baseline --project-name waferfix_baseline --proportion 0.25 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
@@ -9,7 +18,3 @@
 # CUDA_VISIBLE_DEVICES=0 python -m train_baseline --project-name waferfix_baseline --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
 # CUDA_VISIBLE_DEVICES=0 python -m train_baseline --project-name waferfix_baseline --proportion 0.01 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
 
-CUDA_VISIBLE_DEVICES=1 python -m train_baseline --K 1 --project-name waferfix_baseline --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=1 python -m train_baseline --K 2 --project-name waferfix_baseline --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=1 python -m train_baseline --K 5 --project-name waferfix_baseline --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=1 python -m train_baseline --K 3 --project-name waferfix_baseline --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
--- run_baseline_pretrained.sh
+++ run_baseline_pretrained.sh
@@ -1,14 +1,16 @@
 # nohup ./run_baseline_pretrained.sh > nohup_baseline_pretrained.out &
 # tail -f nohup_baseline_pretrained.out 
 
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 1.0  --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 0.50 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 0.25 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 0.1  --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --project-name waferfix_baseline_pretrained --proportion 0.01 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
 
-CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 1 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 2 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 5 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
-CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 3 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+for p in 0.05 0.1 0.25 0.5 1.0
+do
+    for k in 1 2 3 5
+    do
+        CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K $k --project-name waferfix_baseline_pretrained --proportion $p --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+    done
+done
+
+
+# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 2 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 5 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+# CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K 3 --project-name waferfix_baseline_pretrained --proportion 0.05 --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
