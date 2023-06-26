7893b8b1754c9b664b72e4f0b8c680b0fc637c56
user <verystrongjoe@gmail.com>
2023-06-26 09:41:21 +0900

    add experiment with n 3 or more

--- run_0.sh
+++ run_0.sh
@@ -1,27 +1,25 @@
 # nohup ./run_0.sh > nohup0.out &
 # tail -f nohup0.out 
 
-epoch=150
+epoch=180
 lr=0.01
 arch=resnet18
 limit_unlabled=300000
-proportion=0.05
 aug_types="crop, cutout, noise, rotate, shift"                   # 'crop','cutout','noise','rotate','shift'
 
 l=0.217
 th=0.893
 mu=4
 tau=0.493
-n=2
+seed=1111
 
-pn_main=wm
-pn=$pn_main-$arch-lr-$lr-l-$l-th-$th-mu-$mu-tau-$tau-n-$n-rotate-in-weak
+pn_main=wm_230626
 
-
-for seed in 2345 1234 3456 4567 5678
+for n in 3 4 5
 do
     for prop in 0.05 0.1 0.25 0.50 1.0
     do
-        CUDA_VISIBLE_DEVICES=0 python -m train --rotate-weak-aug --limit-unlabled $limit_unlabled --aug_types $aug_types --mu $mu --lambda-u $l --nm-optim sgd --epochs $epoch --tau $tau --gpus 1 --project-name $pn  --keep --n-weaks-combinations $n --threshold $th --wandb  --dataset wm811k --proportion $prop --arch $arch --batch-size 256 --lr $lr --seed $seed 
+        pn=$pn_main-$arch-lr-$lr-l-$l-th-$th-mu-$mu-tau-$tau-n-$n
+        CUDA_VISIBLE_DEVICES=0 python -m train --limit-unlabled $limit_unlabled --aug_types $aug_types --mu $mu --lambda-u $l --nm-optim sgd --epochs $epoch --tau $tau --gpus 1 --project-name $pn  --keep --n-weaks-combinations $n --threshold $th --wandb  --dataset wm811k --proportion $prop --arch $arch --batch-size 256 --lr $lr --seed $seed 
     done
 done
--- run_baseline_pretrained_0.sh
+++ run_baseline_pretrained_0.sh
@@ -1,12 +1,18 @@
 # nohup ./run_baseline_pretrained_0.sh > nohup_baseline_pretrained_0.out &
 # tail -f nohup_baseline_pretrained_0.out 
 
+l=0.217
+th=0.893
+mu=4
+tau=0.493
+n=2
 
-for p in 0.05 # 0.1 0.25 0.5 1.0
+
+for seed in 2345 1234 3456 4567 5678
 do
-    for k in 1 2 3 5
+    for prop in 0.05 0.1 0.25 0.50 1.0
     do
-        CUDA_VISIBLE_DEVICES=0 python -m train_baseline_pretrained --K $k --project-name waferfix_baseline_pretrained5 --proportion $p --arch resnet18 --batch-size 256 --lr 0.003  --seed 5
+        CUDA_VISIBLE_DEVICES=1 python -m train --limit-unlabled $limit_unlabled --aug_types $aug_types --mu $mu --lambda-u $l --nm-optim sgd --epochs $epoch --tau $tau --gpus 1 --project-name $pn  --keep --n-weaks-combinations $n --threshold $th --wandb  --dataset wm811k --proportion $prop --arch $arch --batch-size 256 --lr $lr --seed $seed 
     done
 done
 
